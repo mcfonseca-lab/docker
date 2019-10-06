977edda26c25cc93ef51074fef6bf2e96bc70b10
Pedro Barbosa <pspbedrobarbosa@gmail.com>
2019-10-06 14:25:35 +0100

    Set significance thresholds in rMATs processing script

--- rmats/run_maser.R
+++ rmats/run_maser.R
@@ -3,12 +3,16 @@ library(maser)
 args = commandArgs(trailingOnly=TRUE)
 label1 = args[[1]]
 label2 = args[[2]]
+min_avg_reads = args[[3]]
+fdr_threshold = args[[4]]
+deltaPSI_threshold = args[[5]]
+
 ########################################
 ############# maser object #############
 ########################################
 maser_obj <- maser(".", c(label1, label2), ftype = "JC")
-maser_coverage_filt <- filterByCoverage(maser_obj, avg_reads = 5)
-maser_top_events <- topEvents(maser_coverage_filt, fdr = 0.05, deltaPSI = 0.1)
+maser_coverage_filt <- filterByCoverage(maser_obj, avg_reads = min_avg_reads)
+maser_top_events <- topEvents(maser_coverage_filt, fdr = fdr_threshold, deltaPSI = deltaPSI_threshold)
 print(maser_top_events)
 
 for (event in c("SE","A5SS","A3SS","RI","MXE")){
